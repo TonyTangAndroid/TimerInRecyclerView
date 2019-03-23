77224c3041031e98439f2987a50b1474b2fdf1db
TonyTandAndroid <tonytangandroid@gmail.com>
2019-03-23 16:26:43 -0700

    Make the recycler view visible.

--- app/src/main/java/com/mani/rc/JavaFragment.java
+++ app/src/main/java/com/mani/rc/JavaFragment.java
@@ -1,8 +1,44 @@
 package com.mani.rc;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 public class JavaFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_recycler_view, container, false);
+        recyclerView = rootView.findViewById(R.id.recycler_view);
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initUI();
+        bindAdapter();
+    }
+
+    private void initUI() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+    }
+
+    private void bindAdapter() {
+        RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter();
+        recyclerView.setAdapter(recyclerViewAdapter);
+    }
 }
