7d70a89b3b583699cbc65262ecbfe339839197d1
TonyTandAndroid <tonytangandroid@gmail.com>
2019-03-23 16:04:16 -0700

    Prepare to integrate auto dispose

--- app/src/main/java/com/mani/rc/MainActivity.java
+++ app/src/main/java/com/mani/rc/MainActivity.java
@@ -1,8 +1,8 @@
 package com.mani.rc;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
