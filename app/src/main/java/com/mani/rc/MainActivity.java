db00cdd80d0fc9a6d99282fe4980aebab89d5baa
TonyTandAndroid <tonytangandroid@gmail.com>
2019-03-23 16:06:52 -0700

    Add auto dispose dependencies

--- app/src/main/java/com/mani/rc/MainActivity.java
+++ app/src/main/java/com/mani/rc/MainActivity.java
@@ -1,9 +1,9 @@
 package com.mani.rc;
 
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
