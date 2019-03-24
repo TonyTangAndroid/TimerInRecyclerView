aee3aab5e44e2def03e4cd6846d33c2004f150d9
TonyTandAndroid <tonytangandroid@gmail.com>
2019-03-24 06:48:17 -0700

    Add Ticker

---
+++ app/src/main/java/com/mani/rc/Ticker.java
@@ -0,0 +1,45 @@
+package com.mani.rc;
+
+import com.uber.autodispose.ScopeProvider;
+
+import io.reactivex.observers.DisposableObserver;
+
+import static com.uber.autodispose.AutoDispose.autoDisposable;
+
+public class Ticker {
+    private final ScopeProvider provider;
+    private final RxCountdownTimer countdownTimer;
+
+    public Ticker(ScopeProvider provider) {
+        this.provider = provider;
+        this.countdownTimer = new RxCountdownTimer();
+    }
+
+    public void start(long durationInSeconds, Callback callback) {
+        countdownTimer.observe(durationInSeconds).as(autoDisposable(provider)).subscribe(new DisposableObserver<Long>() {
+            @Override
+            public void onNext(Long secondsUntilFinished) {
+                callback.onTick(secondsUntilFinished);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                throw new IllegalStateException(e);
+            }
+
+            @Override
+            public void onComplete() {
+                callback.onFinish();
+            }
+        });
+
+    }
+
+
+    public interface Callback {
+
+        void onTick(long secondsUntilFinished);
+
+        void onFinish();
+    }
+}
